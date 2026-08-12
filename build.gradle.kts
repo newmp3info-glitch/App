8f1e51868128d0b13c6ded1c880a52af6bdf60fd
newmp3info-glitch <newmp3.info@gmail.com>
2026-08-12 15:43:41 +0530

    Clean up build.gradle.kts by removing unused sections
    Removed plugin management and dependency resolution sections.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,5 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.5.0" apply false
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
